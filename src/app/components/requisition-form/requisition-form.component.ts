9356561c756c89d5d0e9958bd11d3839bbf272dd
Kevin Okamoto <kevin.okamoto@sap.com>
2021-01-05 12:52:27 -0800

    Refactor using m-content directive

--- src/app/components/requisition-form/requisition-form.component.ts
+++ src/app/components/requisition-form/requisition-form.component.ts
@@ -1,30 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Requisition } from 'src/app/model/requisition.model';
-import { IMetaRequisition, MetaObjectService } from 'src/app/services/meta-object.service';
+import { MetaObject, MetaObjectService } from 'src/app/services/meta-object.service';
 
 @Component({
   selector: 'app-requisition-form',
   templateUrl: './requisition-form.component.html',
   styleUrls: ['./requisition-form.component.scss'],
 })
-export class RequisitionFormComponent implements OnInit {
+export class RequisitionFormComponent {
 
-  public req: Requisition;
   @Input()
-  set requisition(req: Requisition) {
-    this.req = req;
-    this.metaObjectService.setObject(req);
-  }
+  public req: Requisition;
 
-  public meta$: Observable<IMetaRequisition>;
-
-  constructor(
-    private metaObjectService: MetaObjectService,
-  ) { }
-
-  public ngOnInit(): void {
-    this.meta$ = this.metaObjectService.meta$;
-  }
+  constructor() { }
 
 }
